f795dfc8fd6484c3a3efb468e0d024142e691e7f
Sanjusha Singh <sanjushasingh252@gmail.com>
2025-07-21 02:19:45 +0530

    1st Updation

---
+++ script.js
@@ -0,0 +1,15 @@
+let upBox = document.getElementsByTagName("input")[0];
+let buttons = document.querySelectorAll('button');
+let string = " ";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            upBox.value = string;
+        }
+        string += e.target.innerHTML;
+        upBox.value = string;
+    });
+    
+});
