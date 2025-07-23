70d337fb9ad08212d5d3b340d39913cb46284f22
Sanjusha Singh <sanjushasingh252@gmail.com>
2025-07-23 15:30:08 +0530

    2nd Updation

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ let string = " ";
 let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == 'AC') {
+            string.updateDisplay();
+        }
         if (e.target.innerHTML == '=') {
             string = eval(string);
             upBox.value = string;
         }
         string += e.target.innerHTML;
         upBox.value = string;
+    
+        
     });
     
 });
